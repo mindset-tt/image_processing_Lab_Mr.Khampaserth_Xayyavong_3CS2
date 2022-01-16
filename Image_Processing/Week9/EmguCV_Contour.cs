23f346fa6f981ab70e3a5af7cc6576c77d0a87bb
khampaserth xayyavong <khampaserthxayyavong@hotmail.com>
2022-01-16 13:55:35 +0700

    new update 16/1/2022 1:28PM

--- Image_Processing/Week9/EmguCV_Contour.cs
+++ Image_Processing/Week9/EmguCV_Contour.cs
@@ -35,11 +35,90 @@ namespace WindowsFormsApp1.Week9
 
         private void findContToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            Image<Gray, byte> outputImage;
+            outputImage = inputImage.Convert<Gray, byte>().ThresholdBinaryInv(
+new Gray(150), new Gray(255));
+            VectorOfVectorOfPoint contour = new VectorOfVectorOfPoint();
+            Mat heir = new Mat();
 
+            CvInvoke.FindContours(outputImage, contour, heir, RetrType.External, ChainApproxMethod.ChainApproxSimple);
+            Dictionary<int, double> dict = new Dictionary<int, double>();
+            if (contour.Size > 0)
+            {
+                for (int i = 0; i < contour.Size; i++)
+                {
+                    double area = CvInvoke.ContourArea(contour[i]);
+                    dict.Add(i, area);
+                }
+                //ກຳນົດຈຳນວນ contour ທີ່ຕ້ອງການແຕ້ມ
+                var item = dict.OrderByDescending(v => v.Value).Take(3);
+                foreach (var it in item)
+                {
+                    int key = int.Parse(it.Key.ToString());
+                    //ໃຊ້ແຕ້ມຮູບສີ່ແຈສາກໃສ່ວັດຖຸທີ່ກວດພົບ
+                    Rectangle rect = CvInvoke.BoundingRectangle(contour[key]);
+                    CvInvoke.Rectangle(inputImage, rect, new MCvScalar(255, 0, 0), 3);
+                    //ໃຊ້ແຕ່ໍ້ມ Contour ທົ່ວໄປ
+                    //CvInvoke.DrawContours(inputImage, contour,key, 
+                    //new MCvScalar(255, 0, 0), 2);
+                }
+                Find_Contour.Image = inputImage.ToBitmap();
+
+            }
         }
+            private void shapeDetectionToolStripMenuItem_Click(object sender, EventArgs e)
+            {
+            if (inputImage == null)
+            {
+                return;
+            }
+            int countTriangle = 0;
+            var temp = inputImage.SmoothGaussian(5).Convert<Gray, byte>().ThresholdBinaryInv(new Gray(200), new Gray(255));
+            VectorOfVectorOfPoint contour = new VectorOfVectorOfPoint();
+            Mat m = new Mat();
+            CvInvoke.FindContours(temp, contour, m, RetrType.External, ChainApproxMethod.ChainApproxSimple);
+            for (int i = 0; i < contour.Size; i++)
+            {
+                double perimeter = CvInvoke.ArcLength(contour[i], true);
+                VectorOfPoint approx = new VectorOfPoint();
+                CvInvoke.ApproxPolyDP(contour[i], approx, 0.04 * perimeter, true);
+                CvInvoke.DrawContours(inputImage, contour, i, new MCvScalar(100, 255, 255), 2);
+                Shape_Detection.Image = inputImage.ToBitmap();
 
-        private void shapeDetectionToolStripMenuItem_Click(object sender, EventArgs e)
-        {
+                //Moments center of the shape
+                var moments = CvInvoke.Moments(contour[i]);
+                int x = (int)(moments.M10 / moments.M00);
+                int y = (int)(moments.M01 / moments.M00);
+
+                //ກວດຈຳນວນເມັດ
+                if (approx.Size == 3)
+                {
+                    CvInvoke.PutText(inputImage, "Triangle", new Point(x, y),
+                        FontFace.HersheySimplex, 0.5, new MCvScalar(0, 0, 255), 2);
+                    countTriangle++;
+                }
+                if (approx.Size == 4)
+                {
+                    Rectangle rect = CvInvoke.BoundingRectangle(contour[i]);
+                    double ar = rect.Width / rect.Height;
+                    if (ar == 1)
+                    {
+                        CvInvoke.PutText(inputImage, "Square", new Point(x, y),
+                           FontFace.HersheySimplex, 0.5, new MCvScalar(0, 0, 255), 2);
+                    }
+                    else
+                    {
+                        CvInvoke.PutText(inputImage, "Rectangle", new Point(x, y),
+                           FontFace.HersheySimplex, 0.5, new MCvScalar(0, 0, 255), 2);
+                    }
+                }
+                if (approx.Size == 6)
+                {
+                    CvInvoke.PutText(inputImage, "Hexagon", new Point(x, y),
+                       FontFace.HersheySimplex, 0.5, new MCvScalar(0, 0, 255), 2);
+                }
+            }
+            MessageBox.Show("Number of triangle:" + countTriangle + "\nNumber of Objects:" + contour.Size);
 
         }
 
